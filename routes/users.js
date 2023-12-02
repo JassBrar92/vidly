4ce8835a1a6a9626b29d8a825e4a1f2fddc6c59b
jass <jasbrar080@gmail.com>
2023-12-02 14:24:03 -0800

    using lodash

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const {User, validateUser} = require('../models/user');
+const _=require('lodash');
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
@@ -8,13 +9,11 @@ router.post('/', async (req, res) => {
   if (error) return res.status(400).send(error.details[0].message);
   let user=await User.findOne({email:req.body.email});
   if(user) return res.status(400).send("User already register");
-   user = new User({
-     name: req.body.name,
-     email: req.body.email,
-     password:req.body.password
-    });
+   user = new User(
+    _.pick(req.body,['name','email','password'])
+    );
    await user.save();
-  res.send(user);
+  res.send(_.pick(user,['_id','name','email']));
 });
 
 
