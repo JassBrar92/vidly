3c7adcaf4c5ad9d086c46ccc1668a1925db60522
jass <jasbrar080@gmail.com>
2023-12-02 14:41:53 -0800

    hashing password

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+const bcrypt=require('bcrypt');
 const {User, validateUser} = require('../models/user');
 const _=require('lodash');
 const mongoose = require('mongoose');
@@ -12,6 +13,8 @@ router.post('/', async (req, res) => {
    user = new User(
     _.pick(req.body,['name','email','password'])
     );
+  const salt=await bcrypt.genSalt(10);
+  user.password=await bcrypt.hash(user.password,salt);
    await user.save();
   res.send(_.pick(user,['_id','name','email']));
 });
