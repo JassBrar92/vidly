a249ccacd25c0b8ecdf5e9ca5966e0b169d9fe70
jass <jasbrar080@gmail.com>
2023-12-03 11:18:35 -0800

    authenticating Users

---
+++ routes/auth.js
@@ -0,0 +1,25 @@
+const bcrypt=require('bcrypt');
+const {User} = require('../models/user');
+const _=require('lodash');
+const express = require('express');
+const router = express.Router();
+const Joi =require('joi');
+
+router.post('/', async (req, res) => {
+  const { error } = validate(req.body); 
+  if (error) return res.status(400).send(error.details[0].message);
+  let user=await User.findOne({email:req.body.email});
+  if(!user) return res.status(400).send("Invalid Email or Password");
+  let password=await bcrypt.compare(req.body.password,user.password);
+  if(!password) return res.status(400).send("Invalid Email or Password");
+  res.send(password);
+});
+
+function validate(req){
+  const schema={
+   email:Joi.string().min(5).max(50).required().email(),
+   password:Joi.string().min(5).max(255).required()
+  };
+  return Joi.validate(req,schema);
+}
+module.exports = router;
