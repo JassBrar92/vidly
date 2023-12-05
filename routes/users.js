fc25055b7b99e4664d3f52930115b4a7cf47cb3d
jass <jasbrar080@gmail.com>
2023-12-04 20:03:56 -0800

    setting response header

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,5 @@
+const jwt=require('jsonwebtoken');
+const config=require('config');
 const bcrypt=require('bcrypt');
 const {User, validateUser} = require('../models/user');
 const _=require('lodash');
@@ -16,7 +18,8 @@ router.post('/', async (req, res) => {
   const salt=await bcrypt.genSalt(10);
   user.password=await bcrypt.hash(user.password,salt);
    await user.save();
-  res.send(_.pick(user,['_id','name','email']));
+  const token=jwt.sign({_id:user._id},config.get('jwtPrivateKey'));
+  res.header('x-auth-token',token).send(_.pick(user,['_id','name','email']));
 });
 
 
