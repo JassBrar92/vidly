4d73e91c29750adf7208743a5eaa6dade86a43b2
jass <jasbrar080@gmail.com>
2023-12-06 19:37:10 -0800

    role based authorization

--- models/user.js
+++ models/user.js
@@ -2,6 +2,7 @@ const jwt=require('jsonwebtoken');
 const config=require('config');
 const Joi = require('joi');
 const mongoose = require('mongoose');
+const boolean = require('joi/lib/types/boolean');
 const userSchema=new mongoose.Schema({
   name: {
     type: String,
@@ -21,10 +22,11 @@ const userSchema=new mongoose.Schema({
     required: true,
     minlength: 5,
     maxlength: 1024,
-  }
+  },
+  isAdmin:Boolean
 });
 userSchema.methods.generateAuthToken=function(){
- const token=jwt.sign({_id:this._id},config.get('jwtPrivateKey'));
+ const token=jwt.sign({_id:this._id,isAdmin:this.isAdmin},config.get('jwtPrivateKey'));
  return token;
 }
 const User = mongoose.model('User', userSchema);
