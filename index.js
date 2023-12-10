a7a282cf05f59765153536bde2ac62da77bcd351
jass <jasbrar080@gmail.com>
2023-12-10 11:33:35 -0800

    uncaught Exception

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const winston=require('winston');
+//require('winston-mongodb');
 const err=require('./middleware/error');
 const Joi=require('joi');
 Joi.objectId=require('joi-objectid')(Joi);
@@ -13,7 +14,13 @@ const config=require('config');
 const express = require('express');
 const { error } = require('joi/lib/types/lazy');
 const app = express();
+process.on('uncaughtException',(ex)=>{
+  console.log('Uncaught Exception');
+  winston.error(ex.message,ex);
+});
 winston.add(winston.transports.File,{filename:'logfile.log'});
+//winston.add(winston.transports.MongoDB,{db:'mongodb://localhost:vidly'});
+throw new Error("Something failed while startup");
 if(!config.get('jwtPrivateKey')){
   console.error('Fatal Error: jwtPrivateKey is not defined.');
   process.exit(1);
